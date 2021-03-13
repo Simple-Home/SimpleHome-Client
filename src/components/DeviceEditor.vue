b4bf29df7b610a1261724b48177c2900b2ea3463
milansav <milan.savitskiy@email.cz>
2021-03-13 19:43:34 +0100

    DeviceEditor fixes

--- src/components/DeviceEditor.vue
+++ src/components/DeviceEditor.vue
@@ -4,10 +4,10 @@
             <router-link :to="/device-detail/ + this.$route.params.id"><i class="fa fa-angle-left"> Back</i></router-link>
         </div>
 
-        <h4>Currently editing: {{device.name}}</h4>
+        <h4>Currently editing: {{originalData.data.name}}</h4>
         <div style="display: flex">
-            <p>{{deviceData.name}}</p>
-            <input type="text" v-model="deviceData.name">
+            <p>Name: {{deviceData.data.name}}</p>
+            <input type="text" v-model="deviceData.data.name">
         </div> 
         <button v-on:click="save()">Save</button>
         
@@ -20,6 +20,13 @@ export default {
         return {
             data: null,
             loaded: false,
+            originalData: {
+                data: {
+                    icon: '',
+                    name: '-',
+                    room_id: 0
+                }
+            },
             deviceData: {
                 id: 0,
                 data: {
@@ -32,11 +39,15 @@ export default {
     },
     methods: {
         save () {
-            console.log(this.deviceData);
+            //console.log(this.deviceData);
+            let widgetData = {};
+
+                        
+
             this.$store.dispatch('updateWidget', this.deviceData);
         },
     },
-    created() {
+    created: function() {
 
         this.deviceData.id = this.$route.params.id;
 
@@ -61,43 +72,47 @@ export default {
         .then((data) => {
             this.data = data;
             this.loaded = true;
-        })
+        }).then(() => {
+
+        
+        let ret = {
+                icon: '',
+                name: '-',
+                room_id: 1
+        };
+
+        if(this.loaded && this.$store.state.rooms.length){
+
+            let widgets = this.$store.state.rooms.find(room => room.room_id == this.data.room_id).widgets;
+
+            if(widgets !== undefined){
+                Object.keys(widgets).forEach(key => {
+                    if(widgets[key].subdevice_id == this.$route.params.id){
+                            ret = widgets[key];
+                    }
+                })
+            }
+        }
+        
+        this.originalData.data.name = ret.name;
+        this.originalData.data.icon = ret.icon;
+        this.originalData.data.room_id = ret.room_id;
+
+        this.deviceData.data.name = ret.name;
+        this.deviceData.data.icon = ret.icon;
+        this.deviceData.data.room_id = ret.room_id;        
+
+        });
     },
     computed: {
-        room() {
+
+        room() 
+        {
             if(this.loaded && this.$store.state.rooms.length){
                 return this.$store.state.rooms.find(room => room.room_id === this.data.room_id).name;
             }
 
             return '';
-       },
-        device() {
-            let ret = {
-                    icon: '',
-                    name: '-',
-                    room_id: 0
-            };
-
-            if(this.loaded && this.$store.state.rooms.length){
-
-                let widgets = this.$store.state.rooms.find(room => room.room_id == this.data.room_id).widgets;
-
-                if(widgets !== undefined){
-                    Object.keys(widgets).forEach(key => {
-                        if(widgets[key].subdevice_id == this.$route.params.id){
-                                ret = widgets[key];
-                        }
-                    })
-                }
-            }
-
-            console.log(ret);
-
-            this.deviceData.data.name = ret.name;
-            this.deviceData.data.icon = ret.icon;            
-            this.deviceData.data.room_id = ret.room_id;
-
-            return ret;
         }
     }
 }
